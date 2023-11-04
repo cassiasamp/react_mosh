440c1a9d17eb33ed195855a26a29a7fcd5daf5a0
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 18:22:51 -0300

    adding const name

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,10 @@
 // PascalCasing
 function Message() {
-    // JSX: JavaScript XML
-    // jsx gets compiled to javascript by react -- you can see it in babel
+    const name = 'Cassia';
+    if (name)
+        // JSX: JavaScript XML
+        // jsx gets compiled to javascript by react -- you can see it in babel
+        return <h1>Hello {name}!</h1>;
     return <h1>Hello World!</h1>;
 }
 
