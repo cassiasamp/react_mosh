e2c5221ad6daf5582dbdf15afafff50d8a523940
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 17:49:02 -0300

    first commit of react tutorial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message";
+
+function App(){
+  // self closing syntex in Message component />
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    // jsx gets compiled to javascript by react -- you can see it in babel
+    return <h1>Hello World!</h1>;
+}
+
+export default Message;
