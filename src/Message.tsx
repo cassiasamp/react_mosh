664159e014677b3575738a7ec88cb9ab2dbc9649
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 19:39:27 -0300

    making bootstrap changes work by removing the exclamation point

--- src/Message.tsx
+++ src/Message.tsx
@@ -4,8 +4,8 @@ function Message() {
     if (name)
         // JSX: JavaScript XML
         // jsx gets compiled to javascript by react -- you can see it in babel
-        return <h1>Hello {name}!</h1>;
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World!</h1>;
 }
 
-export default Messagenpm;
+export default Message;
