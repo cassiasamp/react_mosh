bc91efb211b6f9cc1966d9c842a8f02be0673558
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 20:24:36 -0300

    handle click function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 // import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   // deduplicationg previous code
   return (
@@ -10,9 +13,15 @@ function ListGroup() {
       <h1>List </h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           // in an API -> item.id
-          <li className="list-group-item" key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
