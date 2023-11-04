82924c0d0fa8ba81245102a70c13b14d4a2d4157
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 20:04:21 -0300

    condition for no items found

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,27 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
+  let items = [
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris'
+  ];
+
+   items = [];
+
+   if (items.length === 0)
+    return <p>No item found</p>;
+
   return (
     <>
-        <h1>List 1</h1>
+        <h1>List </h1>
         <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+            {items.map((item) => (
+                // in an API -> item.id
+                <li key={item}>{item}</li>
+            ))}
         </ul>
     </>
   );
