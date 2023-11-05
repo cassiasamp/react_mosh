74a54f07895e6219538c7d9de85e267252101d27
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 22:08:46 -0300

    adding close to alert with close button

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -6,7 +6,10 @@ interface Props{
 
 const Alert = ({ children } : Props) => {
   return(
-    <div className="alert alert-primary">{children}</div>
+    <div className="alert alert-primary alert-dismissible">
+        {children}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
   );
 }
 
