0a1540c8c0f14980b9f53fef69cbd020a36d21a2
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 21:34:18 -0300

    changing from text to children

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,11 @@
 
 interface Props{
-  text: string;
+  children: string;
 }
 
-const Alert = ({ text } : Props) => {
+const Alert = ({ children } : Props) => {
   return(
-    <div className="alert alert-primary">{text}</div>
+    <div className="alert alert-primary">{children}</div>
   );
 }
 
