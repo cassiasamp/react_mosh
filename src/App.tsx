c90766148fb2cf3b9ee46132efc637717576f1ed
Cássia Sampaio <cassiasampaios@gmail.com>
2023-11-04 22:04:25 -0300

    clicking button and showing alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App(){
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return(
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Button color='primary' onClick={() => console.log('Clicked')}>My Button</Button>
+      { alertVisible && <Alert>My Alert</Alert>}
+      <Button color='primary' onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
